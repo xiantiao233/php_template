9e4ca3938e6fd2190758178db0493000372a60b7
xiantiao233 <112249833+xiantiao233@users.noreply.github.com>
2024-06-02 11:34:51 +0800

    Update all.php

--- function/all.php
+++ function/all.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__ . '/info/Client.php';
+require_once __DIR__ . '/info/Status.php';
+require_once __DIR__ . '/info/XtBack.php';
 require_once __DIR__.'/AccessRestrictions.php';
 
 require_once __DIR__.'/../script/onLoading.php';
